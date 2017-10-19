8532d04800366fd48c6ea8bec9041217d2bcd778
Leonardo Nicolas <leonicolas@gmail.com>
2017-10-19 21:41:49 -0200

    Add support to Webpack and Babel

---
+++ src/assets/js/main.js
@@ -0,0 +1,13 @@
+console.log('oi');
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+
+console.log(canvas, context);
+const config = {
+  screen: {
+    width: 512,
+    height: 384
+  }
+};
+
+context.fillRect(0, 0, config.screen.width, config.screen.height);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  },
+
+  context: path.resolve('src'),
+  entry: './assets/js/main.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: './assets/js/main.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'index.html'
+    })
+  ]
+};
