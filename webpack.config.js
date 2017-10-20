2d0c19f4eb49a440ace792e7bae54ad0cdee5fef
Leonardo Nicolas <leonicolas@gmail.com>
2017-10-20 10:05:29 -0200

    Add dist directory clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -27,6 +28,7 @@ module.exports = {
   },
 
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       inject: true,
       template: 'index.html'
