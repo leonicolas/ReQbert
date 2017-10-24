ea0ff47cdc2650cdb013007acc9e69f94a96072d
Leonardo Nicolas <leonicolas@gmail.com>
2017-10-23 22:12:21 -0200

    Draw background using SpriteMap class

--- src/app/game.js
+++ src/app/game.js
@@ -1,11 +1,24 @@
 import { loadImage } from './lib/loaders';
 import config from './config.js';
 
+import SpriteMap from './lib/SpriteMap';
+
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 
 context.fillRect(0, 0, config.screen.width, config.screen.height);
 
-loadImage('/assets/img/characters.png').then(image => {
-  context.drawImage(image, 0, 0, 32, 32, 0, 0, 32, 32);
+loadImage('/assets/img/tiles.png').then(image => {
+  const sprites = new SpriteMap(image);
+  sprites.define('brick', 0, 24, 16, 16);
+
+  for (let x = 0; x < config.screen.width; x += 16) {
+    sprites.draw('brick', context, x, 0);
+    sprites.draw('brick', context, x, config.screen.height - 16);
+  }
+
+  for (let y = 16; y < config.screen.height - 16; y += 16) {
+    sprites.draw('brick', context, 0, y);
+    sprites.draw('brick', context, config.screen.width - 16, y);
+  }
 });
