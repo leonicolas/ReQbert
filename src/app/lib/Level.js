78bd28ebe77a0d6dfdef082c5da7d0e5bd304a1f
Leonardo Nicolas <leonicolas@gmail.com>
2019-03-06 10:26:40 -0300

    Prepare level drawing logic.

--- src/app/lib/Level.js
+++ src/app/lib/Level.js
@@ -1,4 +1,5 @@
 import config from '../config.js';
+import Vec2 from './Vec2';
 
 export default class Level {
 
@@ -10,21 +11,33 @@ export default class Level {
 
     const context = this.buffer.getContext('2d');
 
-    const blocks = [
-      "block1-right",
-      "block1-front",
-      "block1-top",
-      "block1-bottom-z",
-      "block1-front-z",
-      "block1-front-x",
-      "block1-top-x",
-      "block1-bottom-x"
-    ];
+    const level1 = {
+      refBlock: "bl1-r",
+      blocks: [
+        [null   , null   , null   , null   , "bl1-f", null   , null   , null   , null   ],
+        [null   , null   , null   , "bl1-f", null   , "bl1-t", null   , null   , null   ],
+        [null   , null   , "bl1-f", null   , "bl1-f", null   , "bl1-t", null   , null   ],
+        [null   , "bl1-t", null   , "bl1-t", null   , "bl1-t", null   , "bl1-f", null   ],
+        ["bl1-f", null   , "bl1-t", null   , "bl1-f", null   , "bl1-t", null   , "bl1-f"],
+        [null   , "bl1-f", null   , "bl1-f", null   , "bl1-t", null   , "bl1-t", null   ],
+        [null   , null   , "bl1-t", null   , "bl1-t", null   , "bl1-f", null   , null   ],
+        [null   , null   , null   , "bl1-f", null   , "bl1-f", null   , null   , null   ],
+        [null   , null   , null   , null   , "bl1-t", null   , null   , null   , null   ],
+      ]
+    };
 
-    let pos = 3;
-    blocks.forEach(blockName => {
-      this.spriteMap.draw(blockName, context, { x: pos, y: 3 });
-      pos += 3;
+    // Draw reference block.
+    this.spriteMap.draw(level1.refBlock, context, new Vec2(3, 3));
+
+    let posX = 3, posY = 3;
+    level1.blocks.forEach(line => {
+      line.forEach(blockName => {
+        if(blockName)
+          this.spriteMap.draw(blockName, context, new Vec2(posX, posY));
+        posX += 3;
+      });
+      posX = 3;
+      posY += 2;
     });
   }
 
