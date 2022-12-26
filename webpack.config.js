2166e3015fb3b98622ee61b8a97051611bb89b00
Leonardo Nicolas <leonicolas@gmail.com>
2022-12-26 11:13:29 -0800

    Initial conversion from javascript to type script

--- webpack.config.js
+++ webpack.config.js
@@ -22,13 +22,40 @@ module.exports = {
   },
 
   context: path.resolve('src'),
-  entry: './index.js',
+  entry: './index.ts',
   output: {
     path: path.resolve('dist'),
     filename: path.join(jsPath, 'main.js')
   },
 
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    extensionAlias: {
+     ".js": [".js", ".ts"],
+     ".cjs": [".cjs", ".cts"],
+     ".mjs": [".mjs", ".mts"]
+    }
+  },
+
   module: {
+    rules: [
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ],
+
     rules: [
       {
         test: /\.js$/,
